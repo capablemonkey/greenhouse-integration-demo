8f9286e70d0a4a9f2e9debaf89675102a0cf5cb6
Gordon Zheng <gordon@greenhouse.io>
2015-11-19 17:30:40 -0500

    add rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntegrationOptionsDemo::Application.config.secret_token = '737b0d2cf198588e2d6c93a0d94f889d3b0b5b969a28a6e30bcf60dd2d17a80b1a976d2ab9766d012fd6ffd7f72881a3c45172e1302f8da1fb7055d08a16da4f'
