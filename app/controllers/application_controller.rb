ed8da8d3486e53a624f4c0ca562d1370d8531715
Gordon Zheng <gordon@greenhouse.io>
2015-11-19 20:17:27 -0500

    discard option2 action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,6 @@ JOB_BOARD_NAME = "vaulttec"
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  def option2
-  	render "option2.erb"
-  end
-
   def option3
   	@offices = (JSON.parse RestClient.get "https://api.greenhouse.io/v1/boards/#{JOB_BOARD_NAME}/embed/offices")["offices"]
   	render "option3.erb"
